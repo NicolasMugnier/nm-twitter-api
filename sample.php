60ac3e4530ca830aeca6f676038ae3dd96524f48
Nicolas Mugnier <6680190+NicolasMugnier@users.noreply.github.com>
2017-02-25 08:20:45 +0000

    Update sample.php

--- sample.php
+++ sample.php
@@ -2,12 +2,18 @@
 
 require_once dirname(__FILE__).'/vendor/autoload.php';
 
+$oauthConsumerKey = '8xIXiMDPRtxNv0IyhrptpESyDOwmEh8FL7NCz6Bg4Csug';
+$oauthConsumerSecret = '5BezU2SwNnWz7dH4JG7XgSN0CpA7srgRp0kiCcFlBu8GhDMdqq';
+$oauthToken = '115008325-uTQ5Bgvt8LHt43bUWyxWti30ok6fw4GRDZrPKJNW';
+$oauthTokenSecret = '8xIXiMDPRtxNv0IyhrptpESyDOwmEh8FL7NCz6Bg4Csug';
+
 $app = new NicolasMugnier\Twitter\App\Application();
 $client = $app->getContainer()->get('twitter_api_statuses_get_home_timeline');
-$response = $client->setOAuthConsumerKey('lubRW6JeF6nSxDiCemEI6qnx9')
-    ->setOAuthConsumerSecret('5BezU2SwNnWz7dH4JG7XgSN0CpA7srgRp0kiCcFlBu8GhDMdqq')
-    ->setOAuthTokenSecret('8xIXiMDPRtxNv0IyhrptpESyDOwmEh8FL7NCz6Bg4Csug')
-    ->setOAuthToken('115008325-uTQ5Bgvt8LHt43bUWyxWti30ok6fw4GRDZrPKJNW')
+$response = $client->setOAuthConsumerKey($oauthConsumerKey)
+    ->setOAuthConsumerSecret($oauthConsumerSecret)
+    ->setOAuthTokenSecret($oauthTokenSecret)
+    ->setOAuthToken($oauthToken)
+    ->setCount(1)
     ->execute();
 
-echo '<pre>';var_dump($response);die('</pre>');
+echo '<pre>';var_dump($response->json());die('</pre>');
